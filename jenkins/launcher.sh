f84bb1fe5694de71662bab80077863696ad99e3b
yuriskltc <70755518+yuriskltc@users.noreply.github.com>
2023-03-13 11:38:30 +0100

    Update launcher.sh

--- jenkins/launcher.sh
+++ jenkins/launcher.sh
@@ -3,5 +3,5 @@ if [ ! -f /opt/scheduled/.deploy.lock ]
 then
     echo "deployment in process, skipping run"
 else
-    echo "for i in `ls /opt/scheduled/*.jar | sort | tail -n 1`"; do java -jar $i Base; done
+    echo "for i in `ls /opt/scheduled/*.jar | sort | tail -n 1`"; do java -jar $i Base; done 
 fi
